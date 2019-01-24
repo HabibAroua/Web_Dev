a21f69941427db8bc457a2926bdb76c4a0ef308f
HabibAroua <habibha.aroua82@gmail.com>
2019-01-24 19:12:34 +0100

    var +

--- js/app.js
+++ js/app.js
@@ -3,3 +3,8 @@ function myClick()
     document.write("Habib Aroua");
     alert("Hello world");
 }
+
+var number1=2;
+var number2=5;
+var result=number1+number2;
+document.write("Result is "+result);
