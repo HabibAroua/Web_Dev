9ed5b310c824286b511e28260ef3e007f8dd2592
HabibAroua <habibha.aroua82@gmail.com>
2019-01-24 20:14:15 +0100

    Learn what is object

--- js/obj.js
+++ js/obj.js
@@ -3,5 +3,17 @@ person=
     name: "Habib" ,
     familyName: "AROUA",
     age: 24
-}
+};
 document.write("\n all information : "+person.name+" "+person.familyName+" "+person.age);
+
+var cat =new Object();
+cat.name="Tananananana";
+cat.age="1";
+cat.color="white";
+document.writeln("<br>");
+document.write("cat information "+cat.name+" "+cat.age+" "+cat.color);
+
+function animal()
+{
+    
+}
