b10115a2b0c499186cc9ff5a8d5645e0e625bec3
HabibAroua <habibha.aroua82@gmail.com>
2019-01-24 20:53:10 +0100

    object + propriete

--- js/obj.js
+++ js/obj.js
@@ -13,7 +13,15 @@ cat.color="white";
 document.writeln("<br>");
 document.write("cat information "+cat.name+" "+cat.age+" "+cat.color);
 
-function animal()
+function myEmploye(id,name, lname,age,dept,salary)
 {
-    
+    this.id=id;
+    this.name=name;
+    this.lname=lname;
+    this.age=age;
+    this.salary=salary;
 }
+
+emp=new myEmploye(1,"Habib","Aroua",24,"Programmer",900000);
+document.write("<br>");
+document.write("all information employee : "+emp.id+" "+emp.name+" "+emp.lname+" "+emp.salary);
