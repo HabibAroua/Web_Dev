1e57709be3dc8acaa3f0770182512bec6ef088dc
HabibAroua <habibha.aroua82@gmail.com>
2019-02-05 20:45:53 +0100

    end date

--- js/date.js
+++ js/date.js
@@ -1,2 +1,15 @@
-var myDate=new Date();
-document.write("The date is "+myDate);
+function time()
+{
+    var myDay=new Date();
+    document.write("The date is "+myDay);
+    var myDate=myDay.getDate();
+    var myMonth=myDay.getMonth();
+    var myYear=myDay.getFullYear();
+    document.write("<br>");
+    document.write("myDay : "+myDate);
+    document.write("<br>");
+    document.write("myMonth : "+myMonth);
+    document.write("<br>");
+    document.write("MyYear : "+myYear);
+}
+time();
