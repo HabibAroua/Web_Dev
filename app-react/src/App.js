bf0015a6c0acf616ddea228ea4f58298f5705b69
Habib Aroua <HabibAroua@users.noreply.github.com>
2019-03-19 12:27:01 +0100

    add the first event

--- app-react/src/App.js
+++ app-react/src/App.js
@@ -1,22 +1,40 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import {x as num1 , y as num2 , Test} from './module'
-import Habib from './module'
-import {default as Testing} from  './module'
-
+import Habib from './module';
+import {default as Testing} from  './module';
+import Child from './Child';
 //import {y} from './module'
 import './App.css';
 
 class App extends Component
 {
-  render()
-  {
-    return (
-      <div className="App">
-        Hello world ! {num1+num2}
-        the function is {Habib()}
-        default is {Testing()}
-      </div>
+    Test()
+    {
+        return " Hello test intern";
+    }
+
+    Test2 =()=>
+    {
+        return " Hello test intern 2";
+    }
+
+    handleClick()
+    {
+        alert("Hello world");
+    }
+    render()
+    {
+        return (
+            <div className="App">
+                Hello world ! {num1+num2}
+                the function is {Habib()}
+                default is {Testing()}
+                <Child />
+                the result the function intern N° 1 : {this.Test()}
+                the result the function intern N° 1 : {this.Test2()}
+                <button onClick={this.handleClick}>Click here</button>
+            </div>
     );
   }
 }
---
+++ app-react/src/Child.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class Child extends Component
+{
+    render()
+    {
+        return(
+            <div>
+                Child Compoment
+            </div>
+        );
+    }
+}
+
+export default Child;
