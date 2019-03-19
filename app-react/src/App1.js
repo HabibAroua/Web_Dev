03ca4c557923d760e8fdabefef5175358c95235d
Habib Aroua <HabibAroua@users.noreply.github.com>
2019-03-20 00:22:07 +0100

    final 20 video

--- app-react/src/App1.js
+++ app-react/src/App1.js
@@ -4,7 +4,8 @@ class App1 extends  Component
 {
     state=
         {
-            name: ''
+            name: '',
+            val:''
         }
 
     handleChange=(e)=>
@@ -19,8 +20,18 @@ class App1 extends  Component
     handleSubmit=(e)=>
     {
         e.preventDefault(e);
-        console.log(this.state.name);
+        console.log(this.state.name+' '+this.state.val);
     }
+
+    handleChange2=(e)=>
+    {
+        this.setState(
+            {
+                val: e.target.value
+            }
+        )
+    }
+
     render()
     {
         return(
@@ -28,11 +39,18 @@ class App1 extends  Component
                 Form part 1
                 <form onSubmit={this.handleSubmit.bind(this)}>
                     <input type="text" onChange={this.handleChange.bind(this)}/>
+                    <select onChange={this.handleChange2}>
+                        <option value="val1">Valeur1</option>
+                        <option value="val2">Valeur2</option>
+                        <option value="val3">Valeur3</option>
+                    </select>
                     <button>Submit</button>
                 </form>
                 {this.state.name}
+                {this.state.val}
             </div>
         )
     }
 }
+
 export default App1;
