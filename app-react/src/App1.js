415f3f8cd8b90c39308e332476a09cb794ad2150
Habib Aroua <HabibAroua@users.noreply.github.com>
2019-03-19 21:56:18 +0100

    add event OnChange()

--- app-react/src/App1.js
+++ app-react/src/App1.js
@@ -2,11 +2,19 @@ import React , {Component} from 'react';
 
 class App1 extends  Component
 {
+    handleChange=(e)=>
+    {
+        console.log(e.target.value);
+    }
     render()
     {
         return(
             <div>
                 Form part 1
+                <form>
+                    <input type="text" onChange={this.handleChange}/>
+                    <button>Submit</button>
+                </form>
             </div>
         )
     }
