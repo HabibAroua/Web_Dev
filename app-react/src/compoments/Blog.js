1cfcf3ec9563c9edb9930b3ff693adf26f636174
Habib Aroua <HabibAroua@users.noreply.github.com>
2019-03-21 12:05:52 +0100

    Show all data in table

--- app-react/src/compoments/Blog.js
+++ app-react/src/compoments/Blog.js
@@ -3,12 +3,21 @@ import axios from "axios";
 
 class Blog extends Component
 {
+    state =
+        {
+            users : []
+        }
     componentDidMount()
     {
         axios.get("https://jsonplaceholder.typicode.com/users")
             .then(res=>
             {
-                console.log(res);
+                console.log(res.data);
+                this.setState(
+                    {
+                        users : res.data
+                    }
+                )
             });
     }
 
@@ -18,6 +27,11 @@ class Blog extends Component
             <div>
                 <h2>Blog</h2>
                 <p>This is a blog</p>
+                <div>
+                    <table border="1px">
+                    {this.state.users.map(user=><tr><td>{user.name}</td><td>{user.username}</td></tr>)}
+                    </table>
+                </div>
             </div>
         )
     }
