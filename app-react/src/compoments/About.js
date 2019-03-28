cc65ebc8a287651147b808f3fceafaee6c43f32c
Habib Aroua <HabibAroua@users.noreply.github.com>
2019-03-28 01:34:00 +0100

    send request to the back-end

--- app-react/src/compoments/About.js
+++ app-react/src/compoments/About.js
@@ -1,13 +1,79 @@
-import React from 'react';
+import React , {Component} from 'react';
+import axios from 'axios';
 
-const About = ()=>
+class About extends Component
 {
-    return(
-        <div>
-            <h2>About</h2>
-            <p>This is about section</p>
-        </div>
-    )
+    state={
+        name:'',
+    };
+
+
+    handleChange=event =>
+    {
+        this.setState({name:event.target.value});
+    }
+
+    handleSubmit=event =>
+    {
+        event.preventDefault();
+        alert(this.state.name);
+        const user=
+            {
+                name : this.state.name,
+            }
+        axios.post(`http://192.168.1.8:5000/test`,{user})
+            .then(res=>{
+                console.log(res);
+                alert(res);
+                alert(res.data);
+                console.log(res.data);
+            });
+    }
+
+    sendLogin(e){
+        e.preventDefault();
+        fetch('http://192.168.1.8:5000/test', {
+            method: 'POST',
+            headers: {'Content-Type':'application/json'},
+            body: JSON.stringify({
+                email: this.email.value,
+                password: this.password.value
+            }),
+        }).then(function(response) {
+            alert(response);
+            return response
+        }).then(function(body)
+        {
+            alert(body.email);
+            console.log(body);
+        });
+    }
+    render(){
+        return (
+            <form onSubmit={(event) => this.sendLogin(event)} class="asdf">
+                <input type="text" placeholder="email" ref={(input) => { this.email = input}}/>
+                <input type="password" placeholder="password" ref={(input) => { this.password = input }}/>
+                <input type="submit" placeholder="submit"/>
+            </form>
+        )
+    }
+/*
+    render()
+    {
+        return(
+            <div>
+                <h2>About</h2>
+                <from onSubmit={this.handleSubmit}>
+                    <label>
+                        Person Name :
+                        <input type="text" name="name" onChange={this.handleChange}/>
+                        <button type="submit" onClick={this.handleSubmit} >Submit</button>
+                    </label>
+                </from>
+            </div>
+        );
+    }
+    */
 }
 
 export default About;
