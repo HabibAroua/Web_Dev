35a1f6ed0a7a1643214fce5a67776d63973c16e0
Habib Aroua <HabibAroua@users.noreply.github.com>
2020-06-17 23:27:40 +0100

    Create index.php

---
+++ Ajax/index.php
@@ -0,0 +1,21 @@
+<!doctype html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8" />
+		<title>Poster en Ajax</title>
+		<link rel="stylesheet" href="style.css" />
+		<script src="jquery.js"></script>
+		<script src="script.js"></script>
+	</head>
+	<body>
+		<h2>Ajax</h2>
+		<form method="POST" class="formulaire">
+			<div class="return"></div>
+			<input type="text" placeholder="Votre nom" class="nom"><br>
+			<textarea placeholder="Votre message"></textarea><br>
+			<input type="submit" class="submit" value="Envoyer ...">
+		</form>
+		<div class="afficher"></div>
+	</body>
+</html>
+<!-- https://www.youtube.com/watch?v=bTLB-FA_duE -->
